da9e523167f3fa8e389934977e1df9d8efad4b1e
N. Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-24 03:02:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(emoticon_file, emoticon)
   emoticon_lib = load_library(emoticon_file)
   japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
   japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
-end	end
+end	
 
 def get_english_meaning(file, emoticon)
   source = YAML.load_file(file)
