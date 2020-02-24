3619fdc31028f8a47f36418c6069921eda1f7674
N. Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-24 03:01:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,23 @@
 # require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file)
+  emot_hashes = {'get_meaning' => {}, 'get_emoticon' => {}}
+  YAML.load_file(file).each do |meaning, value|
+    eng = value[0]
+    jap = value[1]
+    emot_hashes['get_meaning'][jap] = meaning
+    emot_hashes['get_emoticon'][eng] = jap
+  end
+  emot_hashes
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, emoticon)
+  source = YAML.load_file(file)
+  jap = source["get_emoticon"][emoticon]
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file, emoticon)
+  source = YAML.load_file(file)
+  load_library(file)
 end
